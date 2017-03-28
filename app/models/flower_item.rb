aa934965b2f6bcc31729f1e2615253fce9fe6ea1
Di Ma <tarynma0204@yahoo.ca>
2017-03-28 06:02:32 +0000

    added_navigator_to_every_page

--- app/models/flower_item.rb
+++ app/models/flower_item.rb
@@ -1,7 +1,7 @@
 class FlowerItem < ApplicationRecord
   validates :name, :location, :category,:price, presence: true
   validates :name, :description, length: {maximum: 500}
-  validates :price, numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :location
   belongs_to :category
   has_many :lineItem
